5712d9bd7877ce26ea82578e48fcda126c4627ac
dukesen <dukesen98@gmail.com>
2024-02-26 03:36:42 +0530

    Updated LC 238

--- src/dsa/medium/ProductOfArrayExceptItself.java
+++ src/dsa/medium/ProductOfArrayExceptItself.java
@@ -3,6 +3,25 @@ package dsa.medium;
 import java.util.Arrays;
 
 public class ProductOfArrayExceptItself {
+
+    // Using O(1) space complexity -- just reducing extra array
+    public int[] productExceptSelfWithoutAuxArray(int[] nums) {
+        int[] prefixArray = new int[nums.length];
+
+        prefixArray[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            prefixArray[i] = prefixArray[i - 1] * nums[i - 1];
+        }
+
+        int prevSuffixValue = 1;
+        for (int i = prefixArray.length - 2; i >= 0 ; i--) {
+           int currentSuffixValue = prevSuffixValue * nums[i + 1];
+           prefixArray[i] = currentSuffixValue * prefixArray[i];
+           prevSuffixValue = currentSuffixValue;
+        }
+
+        return prefixArray;
+    }
     public int[] productExceptSelf(int[] nums) {
         int[] prefixArray = new int[nums.length];
         int[] suffixArray = new int[nums.length];
@@ -26,6 +45,7 @@ public class ProductOfArrayExceptItself {
 
     public static void main(String[] args) {
         int[] ele = new int[]{1, 2, 3, 4};
-        System.out.println(Arrays.toString(new ProductOfArrayExceptItself().productExceptSelf(ele)));
+        System.out.println(Arrays.toString
+                (new ProductOfArrayExceptItself().productExceptSelfWithoutAuxArray(ele)));
     }
 }
